f3d2bced991d1cc02c7b3a7c04294a2d1a3df6c2
Peter Varga <admin@vargapeter.com>
2019-03-21 07:54:31 +0100

    bugfixes

--- index.js
+++ index.js
@@ -6,11 +6,11 @@ const get = (id) => getStorage().find((product) => product.id === id)
 
 const exists = (id) => !!get(id)
 
-const add = (product, quantity) => exists(product.id) ? update(product.id, 'quantity', (p) => p.quantity + quantity || 1) : saveStorage(getStorage().concat({ ...product, quantity: quantity || 1 }));
+const add = (product, quantity) => exists(product.id) ? update(product.id, 'quantity', get(product.id).quantity + 1) : saveStorage(getStorage().concat({ ...product, quantity: quantity || 1 }));
 
 const remove = (id) => saveStorage(getStorage().filter((product) => product.id !== id))
 
-const update = (id, field, value) => saveStorage(getStorage().map((product) => product.id === id ? updateField(product, field, value) : product))
+const update = (id, field, value) => saveStorage(getStorage().map((product) => product.id === id ? ({ ...product, [field]: value }) : product))
 
 const total = (cb) => getStorage().reduce((sum, product) => isCallback(cb) ? cb(sum, product) : (sum += subtotal(product)), 0);
 
@@ -21,7 +21,5 @@ const destroy = () => clearStorage()
 const isCalcable = (product) => (product && product.price && product.quantity)
 
 const isCallback = (cb) => cb && typeof cb === Function
-
-const updateField = (product, field, value) => ({...product, [field]: isCallback(value) ? value(product) : value })
 
 export { list, get, add, remove, update, total, destroy, exists, subtotal };
